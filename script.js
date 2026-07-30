df5d34af3809698ee699945477f14971b02a5d27
Juniper4778 <jnic221@gmail.com>
2026-07-29 18:54:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-
 // Juniper Grove Property Service LLC
 // Version 1.0
 
-console.log("Juniper Grove website loaded.");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
