4a94a50b23acd05af69e516478d3ec4993bf2f90
Juniper4778 <jnic221@gmail.com>
2026-07-29 19:23:22 -0700

    Refactor smooth scroll functionality in script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-// Juniper Grove Property Service LLC
-// Version 1.0
+document.querySelectorAll('a[href^="#"]').forEach(link => {
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+link.addEventListener('click', function(e){
 
-        const target = document.querySelector(this.getAttribute('href'));
+e.preventDefault();
 
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-    });
+const target=document.querySelector(this.getAttribute('href'));
+
+if(target){
+
+target.scrollIntoView({
+
+behavior:'smooth'
+
+});
+
+}
+
+});
+
 });
